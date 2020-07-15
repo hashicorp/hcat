daa4ec413e36173880e40342e1009d22b4ef6720
John Eikenberry <jae@zhar.net>
2020-07-14 19:25:02 -0700

    add high level examples to docs
    Examples run as integration tests which made the setup/teardown a bit
    involved to pollute the example as little as possible.

--- doc.go
+++ doc.go
@@ -8,21 +8,5 @@ data changes to trigger updates to the templates.
 A simple example of how you might use this library to generate the contents of
 a single template, waiting for all its dependencies (external data) to be
 fetched and filled in, then have that content returned.
-
-
-	func WaitTillComplete(tmpl *Template, w *Watcher) ([]byte, error) {
-		r := NewResolver()
-		for {
-			re, err := r.Run(tmpl, w)
-			if re.Complete || err != nil {
-				return re.Contents, err
-			}
-			err = w.Wait(0) // no timeout
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
-
 */
 package hcat
