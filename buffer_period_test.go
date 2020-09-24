c58e075ca7ada65b3cef46e5216faf61540fb090
findkim <kngo@hashicorp.com>
2020-09-24 18:06:40 -0500

    Add more unit tests for buffer periods

--- buffer_period_test.go
+++ buffer_period_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 func TestBufferPeriod(t *testing.T) {
+	t.Parallel() // test takes a few seconds
+
 	testCases := []struct {
 		name           string
 		min            time.Duration
@@ -41,7 +43,6 @@ func TestBufferPeriod(t *testing.T) {
 		t.Run(testCase.name, func(t *testing.T) {
 			tc := testCase
 			t.Parallel()
-			expectedWithin := tc.min + time.Duration(tc.snoozeCount)*tc.snoozeAfter + time.Second
 
 			triggerCh := make(chan string)
 			bufferPeriods := newTimers()
@@ -57,12 +58,15 @@ func TestBufferPeriod(t *testing.T) {
 			// Simulate consecutive calls to resolver.Run(template)
 			go func() {
 				for i := int64(0); i < tc.snoozeCount; i++ {
-					<-time.After(tc.snoozeAfter * time.Second)
-					bufferPeriods.Buffer(tc.name)
+					<-time.After(tc.snoozeAfter)
+					active := bufferPeriods.Buffer(tc.name)
+					assert.True(t, active, "intentionally snoozing before buffer period completes")
 				}
 			}()
 
 			// Test signal is received within expected duration
+			expectedWithin := tc.min + time.Duration(tc.snoozeCount)*tc.snoozeAfter
+			expectedWithin += time.Second // add a second of leniency
 			select {
 			case id := <-triggerCh:
 				assert.Equal(t, tc.name, id, "unexpected id")
@@ -75,6 +79,8 @@ func TestBufferPeriod(t *testing.T) {
 	}
 
 	t.Run("not configured", func(t *testing.T) {
+		t.Parallel()
+
 		triggerCh := make(chan string)
 		bufferPeriods := newTimers()
 		go bufferPeriods.Run(triggerCh)
@@ -88,6 +94,36 @@ func TestBufferPeriod(t *testing.T) {
 			assert.Fail(t, "unexpected ID when no buffer period was added", id)
 		case <-time.After(time.Millisecond):
 			// test passes
+		}
+	})
+
+	t.Run("multiple", func(t *testing.T) {
+		t.Parallel()
+
+		triggerCh := make(chan string, 5)
+		bufferPeriods := newTimers()
+		go bufferPeriods.Run(triggerCh)
+		defer bufferPeriods.Stop()
+
+		first := time.Duration(2 * time.Second)
+		second := time.Duration(4 * time.Second)
+		bufferPeriods.Add(first, first*2, "first")
+		bufferPeriods.Add(second, second*2, "second")
+
+		bufferPeriods.Buffer("first")
+		bufferPeriods.Buffer("second")
+
+		completed := make(chan struct{})
+		go func() {
+			assert.Equal(t, "first", <-triggerCh)
+			assert.Equal(t, "second", <-triggerCh)
+			completed <- struct{}{}
+		}()
+
+		select {
+		case <-time.After(5 * time.Second):
+			assert.Fail(t, "expected both buffer periods to send a signal")
+		case <-completed:
 		}
 	})
 }
