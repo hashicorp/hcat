8c1874e5ee0e1ac4c68ef11e3df83ffe572ba3ed
findkim <kngo@hashicorp.com>
2021-01-26 09:12:52 -0600

    Add more comments

--- consul_v1.go
+++ consul_v1.go
@@ -10,6 +10,9 @@ import (
 
 var errFuncNotImplemented = fmt.Errorf("function is not implemented")
 
+// FuncMapConsulV1 is a set of template functions for querying Consul endpoints.
+// The functions support Consul v1 API filter expressions and Consul enterprise
+// namespaces.
 func FuncMapConsulV1() template.FuncMap {
 	return template.FuncMap{
 		"service": v1ServiceFunc,
@@ -24,14 +27,18 @@ func FuncMapConsulV1() template.FuncMap {
 	}
 }
 
+// v1TODOFunc is a placeholder function to return an error instead of inheriting
+// the default template functions.
 func v1TODOFunc(recall Recaller) interface{} {
 	return func(s ...string) (interface{}, error) {
 		return nil, errFuncNotImplemented
 	}
 }
 
-// v1ServiceFunc returns or accumulates health service dependencies.
-// /v1/health/service/:service
+// v1ServiceFunc returns or accumulates health information of Consul services.
+//
+// Endpoint: /v1/health/service/:service
+// Template: {{ service "serviceName" <filter options> ... }}
 func v1ServiceFunc(recall Recaller) interface{} {
 	return func(service string, opts ...string) ([]*dep.HealthService, error) {
 		result := []*dep.HealthService{}
@@ -53,7 +60,12 @@ func v1ServiceFunc(recall Recaller) interface{} {
 	}
 }
 
-// v1ConnectFunc returns or accumulates health connect dependencies.
+// v1ConnectFunc returns or accumulates health information of datacenter nodes
+// and its services that are in Consul Connect, the the service mesh collection
+// of Consul.
+//
+// Endpoint: /v1/health/connect/:service
+// Template: {{ connect "serviceName" <filter options> ... }}
 func v1ConnectFunc(recall Recaller) interface{} {
 	return func(service string, opts ...string) ([]*dep.HealthService, error) {
 		result := []*dep.HealthService{}
