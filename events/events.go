414ef43a63dad5676d15c3e706b843d4d937526d
John Eikenberry <jae@zhar.net>
2022-11-01 12:59:16 -0700

    reorder fields for good memory alignment
    I think events are common and ephemeral enough to warrant a memory
    optimization.

--- events/events.go
+++ events/events.go
@@ -12,94 +12,94 @@ type Event interface {
 
 // Trace is useful to see some details of what's going on
 type Trace struct {
+	event
 	ID      string
 	Message string
-	event
 }
 
 // BlockingWait means a blocking query was made
 type BlockingWait struct {
-	ID string
 	event
+	ID string
 }
 
 // ServerContacted indicates that the tracked service has been successfully
 // contacted (received a non-error response).
 type ServerContacted struct {
-	ID string
 	event
+	ID string
 }
 
 // ServerError indicates that an tracked service has been contacted but with
 // an error returned.
 type ServerError struct {
-	ID    string
-	Error error
 	event
+	Error error
+	ID    string
 }
 
 // ServerTimeout indicates that a call to the server timed out.
 type ServerTimeout struct {
-	ID string
 	event
+	ID string
 }
 
 // RetryAttempt indicates that a tracked call is being retried.
 type RetryAttempt struct {
+	event
+	Error   error
 	ID      string
 	Attempt int
 	Sleep   time.Duration
-	Error   error
-	event
 }
 
 // MaxRetries indicates that the maximum number of retries has been reached
 // (and failed).
 type MaxRetries struct {
+	event
 	ID    string
 	Count int
-	event
 }
 
 // NewData indicates that fresh/new data has been retrieved from the service.
 type NewData struct {
-	ID   string
-	Data interface{}
 	event
+	Data interface{}
+	ID   string
 }
 
 // StaleData indicates that the service returned stale (possibly old) data.
 type StaleData struct {
+	event
 	ID          string
 	LastContant time.Duration
-	event
 }
 
 // NoNewData indicates that data was retrieved from the service, but that it
 // matches the current data so no change would be triggered.
 type NoNewData struct {
-	ID string
 	event
+	ID string
 }
 
 // TrackStart indicates that a new data point is being tracked.
 type TrackStart struct {
-	ID string
 	event
+	ID string
 }
 
 // TrackStop indicates that a data point is no longer being tracked.
 type TrackStop struct {
-	ID string
 	event
+	ID string
 }
 
 // Not used yet, need an PolllingQuery interface to match on
 // see BlockingQuery for how it should work
 type PollingWait struct {
+	event
 	ID       string
 	Duration time.Duration
-	event
 }
 
 // Event interface type fulfillment
